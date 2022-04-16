99f36a5d1e4740670f2feea84435d338490c28e3
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2022-04-16 12:53:29 +0100

    added

--- pages/Swap.jsx
+++ pages/Swap.jsx
@@ -6,7 +6,7 @@ import Transaction from "../components/Transaction";
 function Swap() {
   return (
     <div className="bg-[#16161D] py-12">
-      <div className="max-w-[70%] h-[10%]  space-x-8 py-12 grid grid-cols-2  mx-auto">
+      <div className="max-w-[70%] h-[10%]  space-x-8 py-12 grid grid-cols-1 lg:grid-cols-2  mx-auto">
           <div className="col-span-1"><SwapTable /></div>
           <div className="w-full h-fit pb-12 xchange_glass">
               <Swaps />
